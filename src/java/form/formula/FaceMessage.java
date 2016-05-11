297e3599785a186f2910c0b50da8d7885fb2ab07
ferloy13 <fer_loy@hotmail.com>
2016-05-11 12:05:34 -0600

    pruebas de 14.59

---
+++ src/java/form/formula/FaceMessage.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package form.formula;
+
+/**
+ *
+ * @author Fer_l
+ */
+class FaceMessage {
+
+    public FaceMessage(String string) {
+    }
+
+    FaceMessage(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change bdy of generated methods, choose Tools | Templates.
+    }
+
+    FaceMessage(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    FaceMessage(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
